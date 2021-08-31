519bcfa106cc31a9bfa2e4427e4a8b8815aeed5f
Bryan-Wan <84968862+Bryan-Wan@users.noreply.github.com>
2021-08-31 12:47:05 -0400

    Update collapsable.js

--- collapsable.js
+++ collapsable.js
@@ -19,35 +19,3 @@ for (i = 0; i < coll.length; i++) {
 }
 
 
-
-/*function collap(){
-  var content = document.getElementsByClassName("collapsible");
-  for (i = 0; i < content.length; i++){
-    document.write(content.length);
-
-    document.write(content[i]);
-    content[i].addEventListener("click",   content[i].classList.toggle("edges"));
-    
-  }
-/*
-  if (coll.style.display === "grid") {
-    setTimeout(function(){
-      coll.style.display = "none";
-      content[0].classList.toggle("edges");
-      plusminus.innerHTML = "+";
-
-    }, 0); 
-
-  } else {
-    coll.style.display = "grid";
-    content[0].classList.toggle("edges");
-    plusminus.innerHTML = "-";
-
-  }
-
-
-
-  content[0].classList.toggle("active");
-  content[0].classList.toggle("collapsible2");
-*/
-
